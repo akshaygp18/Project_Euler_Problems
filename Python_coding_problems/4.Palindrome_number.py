f52369858073c84a51edfcbdde34ba299352eb24
akshaygp18 <akshaygp41@gmail.com>
2023-09-16 19:12:37 +0530

    palindrome_number

---
+++ Python_coding_problems/4.Palindrome_number.py
@@ -0,0 +1,20 @@
+def is_palindrome(n):
+    # Convert the number to a string
+    num_str = str(n)
+    
+    # Check if the string is equal to its reverse
+    return num_str == num_str[::-1]
+
+def largest_palindrome_product():
+    largest_palindrome = 0
+
+    for num1 in range(999, 99, -1):
+        for num2 in range(num1, 99, -1):
+            product = num1 * num2
+            if product > largest_palindrome and is_palindrome(product):
+                largest_palindrome = product
+    
+    return largest_palindrome
+
+result = largest_palindrome_product()
+print(result)
